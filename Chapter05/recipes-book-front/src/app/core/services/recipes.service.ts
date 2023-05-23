1d572c7cad031ef2bfaf16272f02478cc2138ed7
Hammad Qayyum <67704372+hammad-qayyum@users.noreply.github.com>
2023-05-23 15:36:36 +0500

    chap5 added

--- Chapter05/recipes-book-front/src/app/core/services/recipes.service.ts
+++ Chapter05/recipes-book-front/src/app/core/services/recipes.service.ts
@@ -1,9 +1,8 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable, of, timer } from 'rxjs';
 import { Recipe } from '../model/recipe.model';
-import { delayWhen, retryWhen, tap} from 'rxjs/operators'
 import { environment } from 'src/environments/environment';
+import { catchError, of } from 'rxjs';
 const BASE_PATH = environment.basePath
 
 @Injectable({
@@ -12,7 +11,8 @@ const BASE_PATH = environment.basePath
 
 export class RecipesService {
 
-  recipes$ = this.http.get<Recipe[]>(`${BASE_PATH}/recipes`);
+  recipes$ = this.http.get<Recipe[]>(`${BASE_PATH}/recipes`).pipe(
+    catchError(error => of([])));
 
   constructor(private http: HttpClient) { }
 }
